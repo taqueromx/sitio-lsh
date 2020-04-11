4d5954a271b4e91f2eb04e09685810ca58a3ea28
Alejandro Longoria <alongoriae@gmail.com>
2020-04-11 12:54:02 -0500

    Update assets, SEO, and delete unused boilerplate

--- src/components/Chat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Chat(props) {
-    return(
-        <h1>Aquí hay puro secreto</h1>
-    )
-}
-
-export default Chat
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home(props) {
-    return(
-        <h1>Que onda raza</h1>
-    )
-}
-
-export default Home
