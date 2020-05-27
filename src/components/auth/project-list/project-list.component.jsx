bcc494f62d812efef25fd0b35c082f3255d71226
SamDino <samuel.cas.96@gmail.com>
2020-05-27 17:23:23 -0500

    Changes

--- src/components/auth/project-list/project-list.component.jsx
+++ src/components/auth/project-list/project-list.component.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Card, Heading, Text } from 'rebass';
+import { Card, Heading, Text, Input, Label } from 'rebass';
 
 import './project-list.style.css';
 
 export const CardList = props => (
     <div className='card-list'>
         {Object.keys(props.projects).map((key,index) => (
-            <Card key={index} width={256}>
-                <Heading>{props.projects[key].name}</Heading>
-                <Text>{props.projects[key].description}</Text>
+            <Card key={index} className='card-container'>
+                <Heading>{props.projects[key].proyecto}</Heading>
+                <Text>{props.projects[key].objetivo}</Text>
+                <Label htmlFor='startingDate'>Hora Inicio</Label>
+                {/* <Input 
+                    id='startingDate'
+                    name='startingDate'
+                    type='date'
+                    value={props.projects[key].fechaInicio}
+                /> */}
             </Card>
         ))}
     </div>
