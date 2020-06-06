f27dc9462ab12b469feae29cfe6ac3ed47298cc3
Marving14 <42250151+Marving14@users.noreply.github.com>
2020-06-06 12:54:46 -0500

    Update Dashboard.js

--- src/components/auth/Dashboard.js
+++ src/components/auth/Dashboard.js
@@ -1,10 +1,10 @@
-import React, { Children } from 'react'
+import React from 'react'
 
 import { useUser } from '../../context/user-context'
 import { useAuth } from '../../context/auth-context'
 
 
-
+import 'react-datepicker/dist/react-datepicker.css'
 import history from '../../services/history'
 
 function Dashboard() {
@@ -21,11 +21,12 @@ function Dashboard() {
             <h1>Dashboard</h1>
             <h3>{user.displayName}</h3>
             <p>{user.email}</p>
-            
             <br />
             <p>Estás en el dashboard y podrás hacer cosas de alumno inscrito.</p>
             <br />
-            <a href='/newProject'><button>Crear Proyecto </button> {"    "} </a>
+            <a href='/newProject'><button>Crear Proyecto </button> </a>
+            <a href='/newGuia'><button>Registrar Guia </button> </a>
+            <a href='/newLider'><button>Registrar Lider </button> </a>
             <button onClick={handleSignOut}>Cerrar sesión</button>
         </div>
     )
