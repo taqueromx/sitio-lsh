5b607193509b7d588a457d45e55deb69c52fb7b3
Marving14 <42250151+Marving14@users.noreply.github.com>
2020-06-06 12:56:14 -0500

    Update index.js

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -4,26 +4,27 @@ import { Switch } from 'react-router-dom'
 import Route from './Route'
 
 import About from '../../components/not_auth/About'
-import Dashboard from '../../components/auth/Dashboard' 
+import Dashboard from '../../components/auth/Dashboard'
+import newProject from '../../components/auth/newProject'
+import newGuia from '../../components/auth/newGuia'
+import newLider from '../../components/auth/newLider'
+
+import '../../styles/styleFormP.scss'; 
 
 
-//import styleFormP from '../src/styles/styleFormP.scss'
-//import NewProject from '../../components/auth/newProject'
-// <Route path="/newProject" component={NewProject} />
-
- 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Dashboard} />
       <Route path="/about" component={About} />
       <Route path="/dashboard" component={Dashboard} />
-     
+      <Route path="/newProject" component={newProject} />
+      <Route path="/newGuia" component={newGuia} />
+      <Route path="/newLider" component={newLider} />
       {/* <Route path="/profile" component={Profile} /> */}
 
       {/* redirect user to Dashboard if route does not exist */}
       <Route component={Dashboard} />
-
     </Switch>
   );
 }
