9a4f2eafc624a4aaa564465bd05d88806b0d22a9
SamDino <samuel.cas.96@gmail.com>
2020-06-10 12:32:34 -0500

    Update project-card.component.jsx

--- src/components/auth/project-card/project-card.component.jsx
+++ src/components/auth/project-card/project-card.component.jsx
@@ -11,12 +11,16 @@ export const ProjectCard = ({project, enrollInProject, firstTime, userType}) => 
         debugger;
         footer = <Box width={1} px={2}>
                     <Label>Lideres asignados: </Label>
-                    <Text>{project.body.lideresAsignados[0]}</Text>
+                    <Text>{'Nombre: '+project.body.lideresAsignados[0]}</Text>
+                    <Text>{'Matricula: '+project.body.lideresAsignados[1]}</Text>
+                    <Text>{'Correo: '+project.body.lideresAsignados[1] + '@itesm.mx'}</Text>
                 </Box>;
     }else if(userType === 'lider' && project.body.guiaAsignados !== undefined){
         footer = <Box width={1} px={2}>
                     <Label>Guia Asignado: </Label>
-                    <Text>{project.body.guiaAsignados[0]}</Text>
+                    <Text>{'Nombre: '+project.body.guiaAsignados[0]}</Text>
+                    <Text>{'Matricula: '+project.body.guiaAsignados[1]}</Text>
+                    <Text>{'Correo: '+project.body.guiaAsignados[1] + '@itesm.mx'}</Text>
                 </Box>;
     }else{
         footer = <Box></Box>;
